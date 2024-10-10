f2eee787e77024e7a06a02eb08910dd571865bbb
Ayesha Naseer <ayeshanaseer19999@gmail.com>
2024-10-10 10:29:44 +0500

    Updated button design and font size

--- src/git_lab08/Calculator.java
+++ src/git_lab08/Calculator.java
@@ -1,5 +1,11 @@
 package git_lab08;
 
+import java.awt.Color;
+import java.awt.Font;
+
+import javax.swing.JButton;
+import javax.swing.JPanel;
+
 public class Calculator {
 
 	 // Method to perform addition
@@ -10,5 +16,12 @@ public class Calculator {
     // Method to perform multiplication
     public int multiplication(int a, int b) {
         return a * b;
+    }
+    public void designUI(JPanel panel, JButton button) {
+        // Change button color to green (Member 1's design)
+        button.setBackground(Color.GREEN);
+
+        // Set font size for buttons (Member 1's design)
+        button.setFont(new Font("Arial", Font.PLAIN, 16));
     }
 }
