8c6c0e112d58c9e277fb155aa59516d950cc3c57
zainabk-design <f223691@cfd.nu.edu.pk>
2024-10-10 10:19:41 +0500

    added validation to addition and multiplication method

--- src/git_lab08/Calculator.java
+++ src/git_lab08/Calculator.java
@@ -1,14 +1,23 @@
 package git_lab08;
 
+
+
 public class Calculator {
 
-	 // Method to perform addition
+    // Method to perform addition with input validation
     public int addition(int a, int b) {
+        if (a < 0 || b < 0) {
+            throw new IllegalArgumentException("Inputs must be non-negative integers.");
+        }
         return a + b;
     }
 
-    // Method to perform multiplication
+    // Method to perform multiplication with input validation
     public int multiplication(int a, int b) {
+        if (a < 0 || b < 0) {
+            throw new IllegalArgumentException("Inputs must be non-negative integers.");
+        }
         return a * b;
     }
+
 }
