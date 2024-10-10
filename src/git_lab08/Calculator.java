081e2ca34118e4e0278a0b9ec9285914ab3bf56e
Ayesha Naseer <ayeshanaseer19999@gmail.com>
2024-10-10 10:13:13 +0500

    Added addition and multiplication methods

--- src/git_lab08/Calculator.java
+++ src/git_lab08/Calculator.java
@@ -2,4 +2,13 @@ package git_lab08;
 
 public class Calculator {
 
+	 // Method to perform addition
+    public int addition(int a, int b) {
+        return a + b;
+    }
+
+    // Method to perform multiplication
+    public int multiplication(int a, int b) {
+        return a * b;
+    }
 }
