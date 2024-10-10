3e4f6cdec8ea843a6b904d768138f21441ee98f0
Ayesha Naseer <ayeshanaseer19999@gmail.com>
2024-10-10 11:13:57 +0500

    added UserRegistration class

--- src/git_lab08/Calculator.java
+++ src/git_lab08/Calculator.java
@@ -1,11 +1,12 @@
 package git_lab08;
 
 
-import java.awt.Color;
-import java.awt.Font;
+import java.awt.GridLayout;
 
 import javax.swing.JButton;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 
 public class Calculator {
---
+++ src/git_lab08/UserRegistration.java
@@ -0,0 +1,9 @@
+package git_lab08;
+
+public class UserRegistration {
+
+	public boolean validateUser(String username, String password, String email) {
+        // Basic placeholder logic for now
+        return false;
+    }
+}
